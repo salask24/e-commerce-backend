e22df40d0fd9bce120f0e07faf0354a1d04f85a4
Kimberly Salas <salask0104@gmail.com>
2021-08-25 11:52:03 -0700

    minor change to tag seed

--- seeds/tag-seeds.js
+++ seeds/tag-seeds.js
@@ -2,10 +2,10 @@ const { Tag } = require('../models');
 
 const tagData = [
     {
-        tag_name: 'rock music',
+        tag_name: 'nude',
     },
     {
-        tag_name: 'pop music',
+        tag_name: 'black',
     },
     {
         tag_name: 'blue',
@@ -23,7 +23,7 @@ const tagData = [
         tag_name: 'gold',
     },
     {
-        tag_name: 'pop culture',
+        tag_name: 'off white',
     },
 ];
 
